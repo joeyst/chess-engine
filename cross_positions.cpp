904ba21f8f1fc6b046bc24446c11939d73261222
joeyst <joeystenbeck@gmail.com>
2022-08-11 19:06:53 -0700

    Add print_board function.

--- cross_positions.cpp
+++ cross_positions.cpp
@@ -1,11 +1,35 @@
 #include "cross_positions.h"
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 HorizontalSituation::HorizontalSituation() : board(0), file(0){}
 HorizontalSituation::HorizontalSituation(uint64_t b, uint8_t f) : board(b), file(f){}
+void HorizontalSituation::print_board() {
+  vector<string> horizontals = {};
+  string horizontal = "";
+  for (int rank = 0; rank < 8; rank++) {
+    horizontal = "\n";
+    horizontal += to_string(rank);
+    horizontal += "   ";
+    for (int file = 0; file < 8; file++) {
+      horizontal += to_string(board & 1);
+      horizontal += " ";
+      board >>= 1;
+    }
+      horizontals.push_back(horizontal);
+      horizontal = "";
+  }
+
+  for (int rank = 0; rank < 8; rank++) cout << horizontals[7 - rank];
+  cout << endl << "\n    0 1 2 3 4 5 6 7\n\n\n";
+}
 
 int main () {
+  auto hs = HorizontalSituation(1, 1);
+  hs.print_board();
   return 0;
 }
--- cross_positions.h
+++ cross_positions.h
@@ -8,5 +8,6 @@ class HorizontalSituation {
     uint8_t file;
     HorizontalSituation();
     HorizontalSituation(uint64_t b, uint8_t f);
+    void print_board();
 };
 #endif
