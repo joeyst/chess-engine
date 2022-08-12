0a346cbadf48e4248c7d02736c0a20e357898457
joeyst <joeystenbeck@gmail.com>
2022-08-11 21:11:35 -0700

    Move logic from cross_positions.cpp to positions.cpp.

--- position.h
+++ position.h
@@ -1,8 +1,37 @@
+#include <cstdint>
 
 
 
 #ifndef POSITION_H
 #define POSITION_H
+class HorizontalSituation {
+  public:
+    uint64_t board;
+    uint8_t file;
+    HorizontalSituation();
+    HorizontalSituation(uint64_t b, uint8_t f);
+    void print_board();
+    uint64_t get_occ_to_right();
+    uint64_t get_occ_to_left();
+    uint64_t get_block_to_right();
+    uint64_t get_block_to_left();
+    uint64_t get_block_L_R();
+};
 
+class VerticalSituation {
+  public:
+    uint64_t board;
+    uint8_t rank;
+    VerticalSituation();
+    VerticalSituation(uint64_t b, uint8_t r);
+    void print_board();
+    uint64_t get_occ_above();
+    uint64_t get_occ_below();
+    uint64_t get_block_above();
+    uint64_t get_block_below();
+    uint64_t get_block_A_B();
+    uint64_t flip_board_to_horizontal();
+    uint64_t flip_board_to_vertical(uint64_t b);
+};
 
 #endif
