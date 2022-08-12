7143218c880a03988b4df71d8f963c96e35c7c30
joeyst <joeystenbeck@gmail.com>
2022-08-11 23:14:00 -0700

    Add generate_horizontal_situations.

--- cross_positions.cpp
+++ cross_positions.cpp
@@ -1,31 +1,40 @@
 #include "position.h"
 #include "masks/horizontal_masks.h"
+#include "cross_positions.h"
 
 using namespace std;
 using namespace Masks;
 
+Situations::Situations() : horizontal_situations({}), vertical_situations({}){}
 
+vector<HorizontalSituation> Situations::generate_horizontal_situations_for_file(int file) {
+  vector<HorizontalSituation> hs = {};
+  int pos;
+  int file_value = (1 << file);
+
+  for (int permutation = 0; permutation < 64; permutation++) {
+    pos = (permutation << 1);
+    if (!(file_value & pos)) hs.push_back(HorizontalSituation(pos, file));
+  }
+  return hs;
+}
+
+vector<HorizontalSituation> Situations::generate_horizontal_situations() {
+  vector<HorizontalSituation> hs = {};
+  for (int file = 0; file < 8; file++) {
+    for (auto situation : generate_horizontal_situations_for_file(file)) hs.push_back(situation);
+  }
+  return hs;
+}
 
 int main () {
-  auto vs = VerticalSituation(0x1000100010001, 1);
-  print_board(vs.board);
-  print_board(vs.flip_board_to_horizontal());
-  vs.board = 0x100010001000100;
-  print_board(vs.board);
-  print_board(vs.flip_board_to_horizontal());
-  print_board(vs.get_block_above());
-  print_board(vs.get_block_below());
-  print_board(vs.get_block_A_B());
-
-  vs.rank = 5;
-  print_board(vs.get_block_above());
-  print_board(vs.get_block_below());
-  print_board(vs.get_block_A_B());
-
-  vs.rank = 4;
-  print_board(vs.get_block_above());
-  print_board(vs.get_block_below());
-  print_board(vs.get_block_A_B());
+  auto situation = Situations();
+  auto situations = situation.generate_horizontal_situations();
+  for (auto s : situations) {
+    std::cout << "File: " << (int)s.file << endl;
+    s.print_board();
+  }
+  cout << situations.size();
 
   return 0;
 }
--- cross_positions.h
+++ cross_positions.h
@@ -1,6 +1,18 @@
 #include <cstdint>
+#include <vector>
+#include "position.h"
 
 #ifndef CROSS_POSITIONS_H
 #define CROSS_POSITIONS_H
+using namespace std;
+
+class Situations {
+  public:
+    vector<HorizontalSituation> horizontal_situations;
+    vector<VerticalSituation> vertical_situations;
+    Situations();
+    vector<HorizontalSituation> generate_horizontal_situations_for_file(int file);
+    vector<HorizontalSituation> generate_horizontal_situations();
+};
 
 #endif
